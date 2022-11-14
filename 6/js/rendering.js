32544cc1f69624d8689d78f125f331672da491d1
Keks <keks@htmlacademy.ru>
2022-11-14 06:55:01 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/rendering.js
@@ -0,0 +1,18 @@
+export const renderingPosts = (data) => {
+  const tempPost = document.querySelector('#picture')
+    .content
+    .querySelector('.picture');
+
+  const photoList = document.createDocumentFragment();
+
+  data.forEach(({url, likes, comments}) => {
+    const post = tempPost.cloneNode(true);
+    post.querySelector('.picture__img').src = url;
+    const photoInfo = post.querySelector('.picture__info');
+    photoInfo.querySelector('.picture__comments').textContent = comments.length;
+    photoInfo.querySelector('.picture__likes').textContent = likes;
+    photoList.appendChild(post);
+  });
+
+  document.querySelector('.pictures').appendChild(photoList);
+};
